723c63aa0b87ea0b5f512984e2628cf27b6ceaa1
Ankit Chohan <ankitchohan61@gmail.com>
2023-11-18 22:25:00 +0530

    Dashboard created

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require("../Routes/User/user");
 const chatRoutes = require("../Routes/Chat/chatRoute");
+const groupchat = require("../Routes/GroupChat/groupChat");
 
 function setupRoutes(app) {
   app.use('/user', userRoutes);
   app.use('/chat', chatRoutes);
+  app.use("/groupchat", groupchat);
 }
 
 module.exports = setupRoutes;
